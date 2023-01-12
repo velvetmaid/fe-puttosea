b40c1b3ece2baf77f71d5ee05c3521e877d8c808
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-01-12 18:44:48 +0700

    first-commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-center text-3xl font-bold underline">Hello world!</h1>
+<p class="text-center">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
